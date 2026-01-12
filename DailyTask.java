85cb07f29120f58a5a30d845c5f800466c3fef73
Cefca <hitooo.9947@gmail.com>
2026-01-12 23:15:20 +0900

    refactor: clean up indentation and add 5-time loop using for-loop

--- DailyTask.java
+++ DailyTask.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
-	public class DailyTask {
-    	public static void main(String[] args) {
-    		System.out.println("数字を入力してください。");
-    		Scanner sc = new Scanner(System.in);
-    		int n = sc.nextInt();
-        	
-        	String result = (n > 0) ? "Positive" : (n < 0) ? "Negative" : "Zero";
-        	
-        	System.out.println(result);
-    	}
-	}
+
+public class DailyTask {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("数字を入力してください。");
+			int n = sc.nextInt();
+			
+			String result = (n > 0) ? "Positive" : (n < 0) ? "Negative" : "Zero";
+			System.out.println(result);
+		}
+		
+		sc.close();
+   	}
+}
